07f3ca03f298c17a12667fefd9ace182a01fcf5c
Peter Vail <peterkvail@gmail.com>
2024-02-26 13:10:27 -0600

    Added basic user database functions

---
+++ BE/db.py
@@ -0,0 +1,35 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["database"] #change this to actual db name
+userDB = mydb["users"]
+
+def does_user_exist(userID):
+    myquery={"userID":userID}
+    user = userDB.find_one(myquery)
+    if user == None:
+        return false
+    return true
+
+
+def add_user(userID,encrypted_password):
+    if not does_user_exist(userID):
+        newUser = {
+            "userID" :userID
+            "password" : encrypted_password
+        }
+        userDB.insert_one(newUser)
+    else:
+        return "user exists" # will probably need to change this to an actual error code system
+
+def remove_user(userID):
+    if does_user_exist(userID):
+        myquery={"userID":userID}
+        user = userDB.find_one(myquery)
+        userDB.remove_one(user)
+    else:
+        return "cant find user"
+
+        
+
+    
