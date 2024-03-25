95cbcd45b2963b6c62f3428e71b65fa662061343
PeterVail1 <peterkvail@gmail.com>
2024-03-24 19:32:58 -0500

    returning project name in /projects

--- BE/server.py
+++ BE/server.py
@@ -166,6 +166,7 @@ def get_projects():
             })
         project_list.append({
             'id': project['projectID'],
+            'projectName' : project['projectName']
             'hardwareSets': hw_sets,
             'users': project['users'],
         })
